7f73b8c303ecb5f2e06bd76316a32325e416669a
Daniel <danielramire1996@gmail.com>
2024-06-27 09:39:19 -0500

    added header hamburger icon

--- src/app/container/header-container/header-container.component.ts
+++ src/app/container/header-container/header-container.component.ts
@@ -2,11 +2,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderBlockComponent } from '../../ui/blocks/header-block/header-block.component';
 import { HeaderContainerFacade } from './header-container.facade';
 import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header-container',
   standalone: true,
-  imports: [HeaderBlockComponent, AsyncPipe],
+  imports: [HeaderBlockComponent, AsyncPipe, MatToolbarModule, MatIconModule, MatMenuModule],
   templateUrl: './header-container.component.html',
 })
 export class HeaderContainerComponent implements OnInit, OnDestroy {
@@ -23,16 +26,14 @@ export class HeaderContainerComponent implements OnInit, OnDestroy {
     this.facade.initSubcristion();
   }
 
-
-  openMenu =():void=> {
+  openMenu = (): void => {
     this.isMenuOpen = true
     this.displayValue = "flex"
     this.menu = "header__menu";
   }
 
-  closeMenu=():void=> {
+  closeMenu = (): void => {
     this.isMenuOpen = false
     this.menu = 'header__menu header__menu--hidden';
   }
-
 }
--- src/app/ui/blocks/header-block/header-block.component.ts
+++ src/app/ui/blocks/header-block/header-block.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header-block',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatToolbarModule, MatIconModule, MatMenuModule, NgIf],
   templateUrl: './header-block.component.html',
-  styleUrl: './header-block.component.css'
+  styleUrls: ['./header-block.component.css']
 })
 export class HeaderBlockComponent {
   @Input() isMenuOpen: boolean;
-  @Input() displayValue : string;
+  @Input() displayValue: string;
   @Input() menu: string;
-  @Input() openMenu : ()=>void;
-  @Input() closeMenu :()=>void;
-
+  @Input() openMenu: () => void;
+  @Input() closeMenu: () => void;
 }
